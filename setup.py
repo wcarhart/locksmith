71667bda563cd49a524607e61d0d804ab365a5da
Will Carhart <wcarhart@sandiego.edu>
2019-02-26 11:49:56 -0800

    Removed extraneous code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
 	name='locksmith',
-	version='0.4',
+	version='0.5',
 	author='Will Carhart',
 	author_email='wcarhart@sandiego.edu',
 	description='Your liaison between repository secrets and the great beyond.',
